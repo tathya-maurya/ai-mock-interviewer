671ba959a6ce75c28bcc04cb62bb6d9e04c7c425
tthym <srajalmaurya0987654321@gmail.com>
2026-05-30 15:24:28 +0530

    Update CORS for new Netlify URL

--- ai-mock-interviewer/src/main/java/com/aiinterviewer/ai_mock_interviewer/config/SecurityConfig.java
+++ ai-mock-interviewer/src/main/java/com/aiinterviewer/ai_mock_interviewer/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://stupendous-frangipane-e85063.netlify.app"
+                "https://cool-melomakarona-1189ef.netlify.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
